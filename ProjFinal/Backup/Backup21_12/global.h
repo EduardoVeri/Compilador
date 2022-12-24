efb9f1c70b15c0b92a514449263b0ba62bf434a7
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2022-12-24 09:37:21 -0300

    Backup realizado de arquivos

--- ProjFinal/Backup/Backup21_12/global.h
+++ ProjFinal/Backup/Backup21_12/global.h
@@ -5,13 +5,13 @@ extern char id[26];
 /* Declaracao da arvore de analise sintatica */
 
 typedef struct NoArvore{
+	struct NoArvore * filho[3]; //No maximo três filhos
+	struct NoArvore * irmao;
 	int numLinha;
 	int tipoNo; //decl(0)  exp(1)
 	int tipoDecl; //if(0) repeat(1) assign(2) read(3) write(4)
 	int nometoken; 
 	char lexema[26]; //Tamanho máximo lexema é de 25
-	struct NoArvore * filho[3]; //No maximo três filhos
-	struct NoArvore * irmao;
 } NoArvore;
 
 
---
+++ ProjFinal/Backup/Backup21_12/script_parser_lexer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+clear
+bison -d arquivo_bison.y
+flex -oarquivo_lex.c entrada.l
+gcc -c arquivo_lex.c 
+gcc -c arvore.c 
+gcc arquivo_bison.tab.c arquivo_lex.o arvore.o -o programa_parser.o -lfl
+./programa_parser.o sort.cm
