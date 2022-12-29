e60e50a1f9f3babce616642fe1975dee0660e95d
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2022-12-28 22:53:14 -0300

    Criação de uma main.c

---
+++ ProjFinal/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "tabelaSimb.h"
+#include "arquivo_bison.tab.h"
+#include "global.h"
+
+int main(void){
+    
+    
+    return 0;
+}
