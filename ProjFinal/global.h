e38873318b9ddb3a0f3a2a442f5041a02c1f6a60
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2023-01-11 20:57:53 -0300

    Alteração na identação

--- ProjFinal/global.h
+++ ProjFinal/global.h
@@ -37,9 +37,9 @@ typedef enum {
     WhileK, // While.
     ReturnINT, // Retorno de função inteira.
     ReturnVOID, // Retorno de função void.
-	NuloDecl, // Nulo
+    NuloDecl, // Nulo
     VarDeclK, // Declaração de variável.
-    VetDeclK, // Declaração de vetor.
+	VetDeclK, // Declaração de vetor.
     FunDeclK, // Declaração de função.
     VarParamK, // Declaração de variável parâmetro.
     VetParamK, // Declaração de vetro parâmetro.
@@ -55,7 +55,7 @@ typedef enum {
     TypeK, // Palavra reservada de tipo.
     VetorK, // Uso de parâmetro vetor.
     AssignK, // Atribuição.
-	NuloExp // Nulo.
+    NuloExp // Nulo.
 } tipoEXP;
 
 /* Declaracao da arvore de analise sintatica */
