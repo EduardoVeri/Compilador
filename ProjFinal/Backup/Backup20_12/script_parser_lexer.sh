a6f8e0c749b975dc9f79a586b24a811c47b2c701
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2023-01-15 21:14:38 -0300

    Apagado pasta backup

--- ProjFinal/Backup/Backup20_12/script_parser_lexer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bison -d arquivo_bison.y
-flex -oarquivo_lex.c entrada.l
-gcc -c arquivo_lex.c 
-gcc -c arvore.c 
-gcc arquivo_bison.tab.c arquivo_lex.o arvore.o -o programa_parser.o -lfl
-./programa_parser.o mdc.cm
