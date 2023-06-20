664c69b0776ec09af26051be834c2f0b98032863
Eduardo Verissimo <verissimo_eduardo@hotmail.com>
2023-06-20 18:07:11 -0300

    Toda a passagem de valores por vetores ok

--- ProjFinal/script_projeto.sh
+++ ProjFinal/script_projeto.sh
@@ -7,5 +7,6 @@ flex -oarquivo_lex.c lexer.l
 mv parser.tab.h ../include
 cd ..
 gcc src/*.c -o bin/compilador -lfl -Iinclude 
-./bin/compilador ./testes/assembly/fatorial_recursivo_vetor.cm -o ./bin/codigoBinario.txt -v -ci -ca 
-#./bin/compilador ./testes/sort.cm -o ./bin/codigoBinario.txt -v -ci -ca 
+#./bin/compilador ./testes/assembly/fatorial_recursivo_vetor.cm -o ./bin/codigoBinario.txt -v -ci -ca 
+./bin/compilador ./testes/sort.cm -o ./bin/codigoBinario.txt -v -ci -ca 
+#./bin/compilador ./testes/assembly/vetor.cm -o ./bin/codigoBinario.txt -v -ci -ca 
