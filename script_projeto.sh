7a56ace860ffa7b15a21ae1c40bc8afb99945a49
eduardoveri <verissimo_eduardo@hotmail.com>
2024-05-30 18:33:57 -0300

    Changed bash script after refactor

--- script_projeto.sh
+++ script_projeto.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
 clear
+
+## Using Cmake ##
 make generate
 rm -r ./build
 mkdir build
 cmake -B ./build 
 cmake --build ./build
 
-cd ./src
-bison -d -oparser.c parser.y
-flex -olexer.c lexer.l
-mv parser.h ../include
-cd ..
-gcc src/*.c -o bin/compilador -lfl -Iinclude 
-#./bin/compilador ./testes/assembly/fatorial_recursivo_vetor.cm -o ./bin/codigoBinario.txt -v -ci -ca 
-#./bin/compilador ./testes/sort.cm -o ./bin/codigoBinario.txt -v -ci -ca 
-#./bin/compilador ./testes/assembly/vetor.cm -o ./bin/codigoBinario.txt -v -ci -ca 
-#./bin/compilador ./testes/mdc.cm -o ./bin/codigoBinario.txt -v -ci -ca 
-#./bin/compilador ./testes/assembly/fatorial_recursivo.cm -o ./bin/codigoBinario.txt -v -ci -ca 
+## Using Command Line ##
+# cd ./src
+# bison -d -oparser.c parser.y
+# flex -olexer.c lexer.l
+# mv parser.h ../include
+# cd ..
+# gcc src/*.c -o bin/compilador -Iinclude 
 
-./bin/compilador ./testes/SO/calculo_media.cm -o ./bin/testes_SO/calculo_media.txt
-./bin/compilador ./testes/SO/num_primo.cm -o ./bin/testes_SO/num_primo.txt 
-./bin/compilador ./testes/SO/potencia.cm -o ./bin/testes_SO/potencia.txt
-./bin/compilador ./testes/SO/mdc.cm -o ./bin/testes_SO/mdc.txt 
-./bin/compilador ./testes/SO/sort.cm -o ./bin/testes_SO/sort.txt
-./bin/compilador ./testes/SO/fatorial_recursivo.cm -o ./bin/testes_SO/fatorial_recursivo.txt
-./bin/compilador ./testes/SO/fatorial_iterativo.cm -o ./bin/testes_SO/fatorial_iterativo.txt
-./bin/compilador ./testes/SO/fibonacci.cm -o ./bin/testes_SO/fibonacci.txt 
-./bin/compilador ./testes/SO/soma_funcao.cm -o ./bin/testes_SO/soma_funcao.txt
-./bin/compilador ./testes/SO/modulo.cm -o ./bin/testes_SO/modulo.txt
+## Some Tests ## 
+
+#./bin/compilador ./test_files/assembly/fatorial_recursivo_vetor.cm -o ./bin/codigoBinario.txt -v -ci -ca 
+#./bin/compilador ./test_files/sort.cm -o ./bin/codigoBinario.txt -v -ci -ca 
+#./bin/compilador ./test_files/assembly/vetor.cm -o ./bin/codigoBinario.txt -v -ci -ca 
+#./bin/compilador ./test_files/mdc.cm -o ./bin/codigoBinario.txt -v -ci -ca 
+#./bin/compilador ./test_files/assembly/fatorial_recursivo.cm -o ./bin/codigoBinario.txt -v -ci -ca 
+
+./bin/compilador ./test_files/SO/calculo_media.cm -o ./bin/testes_SO/calculo_media.txt
+./bin/compilador ./test_files/SO/num_primo.cm -o ./bin/testes_SO/num_primo.txt 
+./bin/compilador ./test_files/SO/potencia.cm -o ./bin/testes_SO/potencia.txt
+./bin/compilador ./test_files/SO/mdc.cm -o ./bin/testes_SO/mdc.txt 
+./bin/compilador ./test_files/SO/sort.cm -o ./bin/testes_SO/sort.txt
+./bin/compilador ./test_files/SO/fatorial_recursivo.cm -o ./bin/testes_SO/fatorial_recursivo.txt
+./bin/compilador ./test_files/SO/fatorial_iterativo.cm -o ./bin/testes_SO/fatorial_iterativo.txt
+./bin/compilador ./test_files/SO/fibonacci.cm -o ./bin/testes_SO/fibonacci.txt 
+./bin/compilador ./test_files/SO/soma_funcao.cm -o ./bin/testes_SO/soma_funcao.txt
+./bin/compilador ./test_files/SO/modulo.cm -o ./bin/testes_SO/modulo.txt
